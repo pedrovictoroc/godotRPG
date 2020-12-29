5cfa6d7a5178e9a5e6c31f6d265f3ab7a158ecd2
pedro.carvalho <pedro.carvalho@clinicasim.com>
2020-12-29 12:47:35 -0300

    adding attack animation and state machine

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,8 +6,20 @@ const FRICTION = 140
 
 var velocity = Vector2.ZERO
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+
+onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+
+func _ready():
+	animationTree.active = true
 
 func getDiferenceInXAxis():
 	if(Input.is_key_pressed(KEY_D)):
@@ -26,6 +38,18 @@ func getDiferenceInYAxis():
 		return 	Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state()
+		
+		ATTACK:
+			attack_state()
+		
+		ROLL:
+			pass
+
+
+func move_state():
 	var input_vector = Vector2.ZERO
 	
 	input_vector.x = getDiferenceInXAxis()
@@ -36,10 +60,21 @@ func _physics_process(delta):
 	if (input_vector != Vector2.ZERO):
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector*MAX_SPEED, ACCELERATION)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 		
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity)	
+
+	if(Input.is_action_just_pressed("attack")):
+		state = ATTACK
+
+func attack_state():
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+	
+func attack_animation_finished():
+	state=MOVE
