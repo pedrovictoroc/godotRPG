c5ed6806979e0ba119ade155d6a6b314a6e720cd
pedro.carvalho <pedro.carvalho@clinicasim.com>
2020-12-29 16:17:18 -0300

    adding hurtbox and layers

--- Grass/Grass.gd
+++ Grass/Grass.gd
@@ -1,17 +1,20 @@
 extends Node2D
 
-func _process(delta):
-	if (Input.is_action_just_pressed("attack")):
-		
-		#Load scene
-		var GrassEffect = load("res://Grass/GrassEffect.tscn")
-		#Get instance
-		var grassEffect = GrassEffect.instance()
-		#Get root scene
-		var mainScene = get_tree().current_scene
-		#Set instance
-		mainScene.add_child(grassEffect)
-		#Set position of instance
-		grassEffect.global_position = global_position
-		
-		queue_free()
+func create_grass_effect():	
+	#Load scene
+	var GrassEffect = load("res://Grass/GrassEffect.tscn")
+	#Get instance
+	var grassEffect = GrassEffect.instance()
+	#Get root scene
+	var mainScene = get_tree().current_scene
+	#Set instance
+	mainScene.add_child(grassEffect)
+	#Set position of instance
+	grassEffect.global_position = global_position
+	
+
+
+func _on_Hurtbox_area_entered(area):
+	print(area)
+	create_grass_effect()
+	queue_free()
